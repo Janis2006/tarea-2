bbbd28599c6346cc09572b68b9a111c21b42cd2e
Janis2006 <janislizethsalgadoperez@gmail.com>
2025-05-14 08:42:03 -0600

    Create eje3.c

---
+++ eje3.c
@@ -0,0 +1,17 @@
+//Ejercicio 3- dado como
+dato una cantidad expresada en dólares, convierta esa cantidad en pesos. Tomar en cuenta que:
+1dolar ↢ 11.96 pesos.//
+
+  #include<stdio.h> 
+#include<math.h>
+int main()
+    {
+       float d=11.96; 
+       float c,r;
+       printf("Igrese la cantidad de efectivo en dólares: ");
+       scanf("%f", &c);
+       r=d*c; 
+       printf("Usted tiene: %f pesos", r);
+       return 0;
+       
+    }
