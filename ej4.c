8ef6b095a4006c72549d6b7c5efcaf0e875becf5
Janis2006 <janislizethsalgadoperez@gmail.com>
2025-05-14 09:04:03 -0600

    Update ej4.c

--- ej4.c
+++ ej4.c
@@ -1,4 +1,16 @@
 //Ejercicio 4- Una persona invierte en un banco una determinada cantidad de dinero y
-a una cierta tasa de inter ́es mensual.//
+a una cierta tasa de inter ́es mensual. Se obtendra la cantidad que obtendra al finalizar el mes//
 
-  
+#include<stdio.h> 
+#include<math.h>
+int main()
+    {
+       float i,t=0.15,r; 
+       printf("Igrese la cantidad de efectivo que desea invertir, recuerde que la tasa de interes es del 15%: ");
+       scanf("%f", &i);
+       t=i*t; 
+       r=i+t;
+       printf("Usted va a resibir: %f al final del mes", r);
+       return 0;
+       
+    }
