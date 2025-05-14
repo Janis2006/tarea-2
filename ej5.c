51341a3381fc1af643dd516b3b071c2eb77e3df0
Janis2006 <janislizethsalgadoperez@gmail.com>
2025-05-14 09:26:17 -0600

    Create ej5.c

---
+++ ej5.c
@@ -0,0 +1,23 @@
+//Ejercicio 5- En un negocio de productos electrodomésticos aplican un descuento del
+8% a todos aquellos clientes cuya compra es superior a $2,500. Dado como
+dato el monto de la compra del cliente, calcule lo que el cliente debe pagar//
+
+#include<stdio.h> 
+#include<math.h>
+int main()
+    {
+       float electrodomestico,pago,d=0.08; 
+       printf("Ingrese el monto de su compra en electrodomésticos:");
+       scanf("%f", &electrodomestico);
+       if(electrodomestico>2500)
+       {
+          d=d*electrodomestico; 
+          pago=electrodomestico-d;
+          printf("Usted debera pagar: %f", pago);
+       }
+       else
+       {
+       printf("Su compra no es acredora al descuento");
+       }
+       return 0;
+    }
