3cda5a9dfd4334588388858353a51a34732f79ef
Janis2006 <janislizethsalgadoperez@gmail.com>
2025-05-15 06:01:24 -0600

    Update ej11.c

--- ej11.c
+++ ej11.c
@@ -1,2 +1,18 @@
 //Ejercicio 11- calcule la suma de los n ́umeros pares
 comprendidos entre el 10 y 50.//
+  
+#include <stdio.h>
+int main() 
+{
+    int s=0,i=10,a;
+    for(i=10;i<=50;i++)
+    {
+        a=i % 2;
+      if(a==0)
+      {
+          s=s+i;
+      }
+    }
+    printf("La suma de los números pares es:%d",s);
+    return 0;
+}
